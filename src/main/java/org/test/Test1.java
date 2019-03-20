ce333801658d7377e9a3aaf06d4bb8dd4f8823ad
your name <2275829964@qq.com>
2019-03-20 18:24:18 +0800

    Initial commit

---
+++ src/main/java/org/test/Test1.java
@@ -0,0 +1,10 @@
+package org.test;
+
+/**
+ * Created by EDZ on 2019/3/20.
+ */
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("欢迎你的到来，加油！");
+    }
+}
